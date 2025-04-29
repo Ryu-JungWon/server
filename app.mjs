bdb52903159962a5d5d9916e78c65644d6f415cf
Ryuzy <ryuzy@kakao.com>
2025-04-30 01:28:48 +0900

    MySQL 세팅

--- app.mjs
+++ app.mjs
@@ -2,6 +2,7 @@ import express from "express";
 import postsRouter from "./router/posts.mjs";
 import authRouter from "./router/auth.mjs";
 import { config } from "./config.mjs";
+import { db } from "./db/database.mjs"; // db 테스트
 
 const app = express();
 
@@ -14,4 +15,6 @@ app.use((req, res, next) => {
   res.sendStatus(404);
 });
 
+// DB연결 확인
+db.getConnection().then((connection) => console.log(connection));
 app.listen(config.host.port);
